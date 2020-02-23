3aa79f2b2e5f68752e5d13035dd22ca47787ef83
vuhiep <nkv.hiep@gmail.com>
2020-02-23 23:15:36 +0700

    update viterbi algorithm

--- correct.py
+++ correct.py
@@ -4,11 +4,14 @@ from viterbi import viterbi_decoder
 def gen_states(obs):
 	states = {}
 	for ob in obs:
-		states[ob] = edit(ob)
+		if ob == '<START>' or ob == '<END>':
+			states[ob] = [ob]
+		else:
+			states[ob] = edit(ob)
 	return states
 
 def correct(text):
-	obs = text.split()
+	obs = ['<START>'] + text.split() + ['<END>']
 	states = gen_states(obs)
 	result = viterbi_decoder(obs, states)
-	return " ".join(result)
+	return " ".join(result[1:-1])
